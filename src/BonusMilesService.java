233977275a45340030298c6ee971ac7530a10937
Aleksandr Madzharov <madzharov-aleks@mail.ru>
2021-12-14 00:58:48 +0500

    Homework-java3

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+   public int calculate (int price, int bonus) {
+
+       return 0;
+   }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 150;
+        int bonus = 20;
+        int mili = price / bonus;
+        System.out.println(mili);
+    }
+}
